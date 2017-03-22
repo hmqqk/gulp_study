30836c9dbf7ed649224a508ab948896f36031b66
xiaqijian <2817062995@qq.com>
2017-03-22 17:21:04 +0800

    add gulp-rename

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify'); // js文件压缩
+var rename = require('gulp-rename'); // 重命名
 
 // js文件压缩
 gulp.task('uglify', function(){
@@ -7,3 +8,10 @@ gulp.task('uglify', function(){
 	.pipe(uglify())
 	.pipe(gulp.dest('dist/js'))
 });
+
+// 重命名
+gulp.task('rename', function(){
+	gulp.src('src/js/hello.js')
+	.pipe(rename('hello.min.js'))
+	.pipe(gulp.dest('dist/js'))
+})
