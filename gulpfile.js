a019535df3b32281c6a878b1b717dd45571c0b35
xiaqijian <2817062995@qq.com>
2017-03-22 17:12:50 +0800

    add gulp-uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,9 @@
-gulpfile.js
+var gulp = require('gulp');
+var uglify = require('gulp-uglify'); // js文件压缩
+
+// js文件压缩
+gulp.task('uglify', function(){
+	gulp.src('src/js/*.js')
+	.pipe(uglify())
+	.pipe(gulp.dest('dist/js'))
+});
