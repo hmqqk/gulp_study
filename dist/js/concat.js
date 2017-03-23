deeab534d3e5f3f31f0d17aaa2ac2a8adf5a233d
xiaqijian <2817062995@qq.com>
2017-03-23 14:21:35 +0800

    add gulp-sourcemaps

--- dist/js/concat.js
+++ dist/js/concat.js
@@ -1,1 +1,27 @@
-function hello(){console.log("this is gulp-uglify")}hello();var arr={a:12,fun:function(){console.log("eeee")}};
+let a = 2;
+let b =5 ;
+
+let hello = require('hello.js')
+
+function add(...values) {
+  let sum = 0;
+
+  for (var val of values) {
+    sum += val;
+  }
+  console.log(a);
+  return sum;
+}
+
+add(2, 5, 3) // 10
+function hello () {
+	console.log('this is gulp-uglify')
+}
+hello()
+var arr = {
+	a: 12,
+	fun: function () {
+		console.log('eeee')
+	}
+}
+//# sourceMappingURL=..maps/concat.js.map
